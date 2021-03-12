a76c1e8d71dbdff432ffc729c73c094db42ff9cc
dmaiolli <denysmaiolli@hotmail.com>
2021-03-12 16:33:42 -0300

    eslint-airbnb, prettier & .editorconfig

---
+++ frontend/prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all', // ultima virgula após objetos/arrays
+  allowParens: 'avoid' // não colocar parenteses entre arrow functions que tenham apenas um parametro
+}
